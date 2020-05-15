a92a96ade5b09e93c28dca3caaa0962139477031
sharad-pizza-hut <sharad.shekar@yum.com>
2020-05-15 15:44:28 -0500

    06 intro matrix multiplication

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,13 +1,24 @@
 mat2 matrixPower(highp mat2 m, int n) {
   
   //Raise the matrix m to nth power
+  highp mat2 res = m;
+  for (int i=1;i<16;i++){
+    if (i < n) {
+    res = res * m;
+    }
+  }
 
-  // For example:
-  //
-  //  matrixPower(m, 2) = m * m
-  //
+  // highp mat2 res = m;
+  // for (int i=1;i<16;i++){
+  //   if (i<n) {
+  //     res = res * m;
+  //   }
+  // }
 
-  return mat2(1.0);  
+
+  return res;  
+  // return mat2(1);  
+
 }
 
 //Do not change this line or the name of the above function
