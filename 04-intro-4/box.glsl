ea1f8478d4b5ac5499df8325edbb00bb491e53c7
sharad-pizza-hut <sharad.shekar@yum.com>
2020-05-15 00:57:23 -0500

    intro-04 box

--- 04-intro-4/box.glsl
+++ 04-intro-4/box.glsl
@@ -1,8 +1,12 @@
 bool inBox(highp vec2 lo, highp vec2 hi, highp vec2 p) {
 
   //Test if the point p is inside the box bounded by [lo, hi]
+  bvec2 less = bvec2(lessThan(lo, p));
+  bvec2 greater = bvec2(greaterThan(hi, p));
+  bool isLess = all(less);
+  bool isGreater = all(greater);
 
-  return false;
+  return all(bvec2(isLess, isGreater));
 }
 
 
